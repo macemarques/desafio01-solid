5e74b93b52274e96dbd2e4aa7814c14a7bf2c3c4
Marcelo Marques <marcelomarques2007@gmail.com>
2021-03-20 14:40:58 -0300

    Desafio 01 - SOLID : Todos testes passaram.

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = <{ user_id: string }>request.params;
+
+      const showUser = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(showUser);
+    } catch (error) {
+      return response.status(404).json({ error: "User does not exists!" });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = <{ user_id: string }>request.params;
+
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(404).json({ error: "User does not exists!" });
+    }
   }
 }
 
